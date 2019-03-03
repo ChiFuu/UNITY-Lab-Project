a3237d2b56d8a3cb4b728dc3c8a9142b0b397b0d
ChiFuu <45012505+ChiFuu@users.noreply.github.com>
2019-03-03 19:06:37 +0100

    3rd lab session

---
+++ TPJ/Assets/Respowner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respowner : MonoBehaviour
+{
+    public float maxHealth;
+    private float currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth=maxHealth;  
+    }
+
+     void OnCollisionEnter(Collision collision)
+        {
+        
+           if (collision.collider.tag=="Bullet")
+            {
+                currentHealth-=20;
+                Debug.Log(currentHealth);
+            }
+            
+        }
+    // Update is called once per frame
+    void Update()
+    {
+       var yposition=this.transform.position.y;
+        if ((yposition<-15 )|| (currentHealth <= 0))
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name,LoadSceneMode.Single);
+        
+        }
+        
+    }
+}
---
+++ TPJ/Assets/Shooter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{   
+    public GameObject bulletprefab;
+    public Transform firepoint;
+    private Rigidbody rb;
+    private GameObject cannonball,cannonball2;
+    
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+    public float Power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+            Destroy(cannonball,2f);
+        }
+    
+    }
+    private GameObject Shoot(){
+        cannonball=Instantiate(bulletprefab,firepoint.position, firepoint.rotation);
+        rb=cannonball.GetComponent<Rigidbody>();
+        rb.AddForce(Power, 0,0, ForceMode.Impulse);
+        return cannonball;
+    }
+}
